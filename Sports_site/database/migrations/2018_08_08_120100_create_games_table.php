c86a7ca8197c9551d484b0319a5756a4c61643fd
wac <antoine.leon@epitech.eu>
2018-08-08 14:04:08 +0200

    created database and tables

---
+++ Sports_site/database/migrations/2018_08_08_120100_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('team1');
+            $table->string('team2');
+            $table->string('score');
+            $table->string('winner');
+            $table->string('game_location');
+            $table->datetime('game_time');
+            $table->integer('number_of_fouls');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}

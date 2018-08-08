9a699a4a92f70aa086f910981e2133e3c7b9beee
wac <antoine.leon@epitech.eu>
2018-08-08 18:30:25 +0200

    Added admin form

--- Sports_site/resources/views/Pages/admin.blade.php
+++ Sports_site/resources/views/Pages/admin.blade.php
@@ -7,8 +7,32 @@
     <title>Document</title>
 </head>
 <body>
-    Welcome to admin page
-
     
+Ajouter une équipe
+
+{!! Form::open(['action' => 'TeamsController@add']) !!}
+
+{!! Form::label('name','name') !!}
+{!! Form::text('name') !!}
+
+{!! Form::label('country','country') !!}
+{!! Form::text('country') !!}
+
+{!! Form::label('flag','flag') !!}
+{!! Form::file('flag') !!}
+
+{!! Form::label('points_per_game','points_per_game') !!}
+{!! Form::number('points_per_game') !!}
+
+{!! Form::label('ball_possession','ball_possession') !!}
+{!! Form::number('ball_possession') !!}
+
+{!! Form::label('team_ranking','team_ranking') !!}
+{!! Form::number('team_ranking') !!}
+
+{!! Form::submit('submit') !!}
+
+{!! Form::close() !!}
+
 </body>
 </html>
